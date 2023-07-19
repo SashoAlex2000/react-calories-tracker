b5e2b38016e38c715016e0af71dc9c4e51d6152c
SashoAlex2000 <sahooo3@abv.bg>
2023-07-19 19:52:04 +0300

    feat: add user auth info preservation in localStorage

--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -1,21 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+// 20230719 -> set/remove the userData to localStorage, and set the initialState to be equal
+// to the data in localStorage, to preserve auth info on refresh
 const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        user: undefined,
+        user: JSON.parse(localStorage.getItem('userData')),
     },
     reducers: {
         setUser (state, action) {
-            state.user = {
+            console.log('test');
+
+            const userData = {
                 uid: action.payload.uid,
                 username: action.payload.username,
                 email: action.payload.email,
             };
+
+            state.user = userData
+            localStorage.setItem('userData', JSON.stringify(userData));
+
         },
         removeUser (state, action) {
             state.user = undefined;
+            localStorage.removeItem('userData')
         },
         addCurrentMetaCounter (state, action) { // 20230607 -> when fetching foods, use this reducer to put the meta counter to the user state
             state.user = {
