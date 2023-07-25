842387f57167f7c09cd8f3b7727912846a1e220b
SashoAlex2000 <sahooo3@abv.bg>
2023-07-25 12:48:14 +0300

    feat: add foodItem choices filter on keystroke and correct initial render

--- src/components/MealChoicesToEat.js
+++ src/components/MealChoicesToEat.js
@@ -1,23 +1,20 @@
 import { useDispatch } from "react-redux";
 import EatFooodItemCard from "./EatFoodItemCard";
-import { daysActions } from "../store/days-slice";
 import classes from './MealChoicesToEat.module.css';
 import { eatFood } from "../store/days-actions";
 import { foodFilterOptions, transformDate } from "../utils/constants";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
-const MealChoicesToEat = (props) => {
+const MealChoicesToEatTEST = (props) => {
 
     const dispatch = useDispatch();
 
-    const [foodChoices, setFoodChoices] = useState(props.currentUserFoods);
     const [selectedSortingParam, setSelectedSortingParam] = useState(Object.entries(foodFilterOptions)[0][0]);
     const [searchedFood, setSearchedFood] = useState('');
-    
+
     // 20230719 -> no longer use addNewFood, dispatch an 'eatFood' action to send the data to Firebase
     const addFoodEatenHandler = (foodItemId, quantity) => {
-        console.log('we really in separate component though');
-        
+
         // prevent empty submission
         if (quantity <= 0) {
             return;
@@ -35,8 +32,47 @@ const MealChoicesToEat = (props) => {
         updatedFoods[foodItemId] += Number(quantity);
 
         dispatch(eatFood(props.userId, date, updatedFoods));
-        
-    }
+
+    };
+
+    // 20230725 -> remove the sorting by button; sort on keystroke and option change
+    // useMemo to memoize the results and re-render only when necessary 
+    const filteredItems = useMemo(() => {
+        if (!searchedFood.trim()) return props.currentUserFoods;
+        return Object.fromEntries(Object.entries(props.currentUserFoods).filter(([key, value]) => value.name.includes(searchedFood)));;
+    }, [
+        props.currentUserFoods, // use props.currentUserFoods to re-render correctly on DB fetch
+        searchedFood
+    ]);
+
+    const sortedItems = useMemo(() => {
+        const itemsCopy = { ...filteredItems };
+
+        if (selectedSortingParam === "date-asc") {
+            return itemsCopy;
+        } else if (selectedSortingParam === "date-desc") {
+            const reversedKeys = Object.keys(itemsCopy).reverse();
+            const newObj = {};
+            reversedKeys.forEach(key => {
+                newObj[key] = props.currentUserFoods[key];
+            });
+            // filter the choices to exclude undefined values
+            return Object.fromEntries(Object.entries(newObj).filter(([key, value]) => value !== undefined));
+        } else if (selectedSortingParam === "calories-desc") {
+            console.log("baznin ga");
+            let foodEntries = Object.entries(itemsCopy);
+            foodEntries.sort((a, b) => b[1].caloriesPerDenom - a[1].caloriesPerDenom);
+            return Object.fromEntries(foodEntries);
+        } else if (selectedSortingParam === "calories-asc") {
+            let foodEntries = Object.entries(itemsCopy);
+            foodEntries.sort((a, b) => a[1].caloriesPerDenom - b[1].caloriesPerDenom);
+            return Object.fromEntries(foodEntries);
+        };
+
+    }, [
+        filteredItems,
+        selectedSortingParam
+    ]);
 
     const handleSortingParamChange = (event) => {
 
@@ -44,52 +80,19 @@ const MealChoicesToEat = (props) => {
 
     };
 
-    const sortButtonHandler = () => {
-
-        let finalFoodChoices;
-
-        // props.currentUserFoods has to be used, to avoid nulling the food choices with
-        // filtering by searched word
-        if (selectedSortingParam === "date-asc") {
-            finalFoodChoices = props.currentUserFoods;
-        } else if (selectedSortingParam === "date-desc") {
-            const reversedKeys = Object.keys(props.currentUserFoods).reverse();
-            const newObj = {};
-            reversedKeys.forEach(key => {
-                newObj[key] = foodChoices[key];
-            });
-            // filter the choices to exclude undefined values
-            finalFoodChoices = Object.fromEntries(Object.entries(newObj).filter(([key, value]) => value !== undefined));
-        } else if (selectedSortingParam === "calories-desc") {
-            console.log("baznin ga");
-            let foodEntries = Object.entries(props.currentUserFoods);
-            foodEntries.sort((a, b) => b[1].caloriesPerDenom - a[1].caloriesPerDenom);
-            finalFoodChoices = Object.fromEntries(foodEntries);
-        } else if (selectedSortingParam === "calories-asc") {
-            let foodEntries = Object.entries(props.currentUserFoods);
-            foodEntries.sort((a, b) => a[1].caloriesPerDenom - b[1].caloriesPerDenom);
-            finalFoodChoices = Object.fromEntries(foodEntries);
-        };
-
-        if (searchedFood.trim() !== '') {
-            finalFoodChoices = Object.fromEntries(Object.entries(finalFoodChoices).filter(([key, value]) => value.name.includes(searchedFood)));
-        };
-
-        setFoodChoices(finalFoodChoices);
-
-    };
-
+    // 20230725 -> the bug with items not re-rendering here when fetched was due to them being in local 
+    // state with useState, which didn't catch anything changed in the store, beyond the inital state,
+    // so there was need for reaching the page again; fixed with using props.currentUserFoods
     return <div>
-        ORder by:
+        Order by:
         <select value={selectedSortingParam} onChange={handleSortingParamChange}>
-                {Object.entries(foodFilterOptions).map(([key, value]) => <option key={key} value={key}>
-                    {value}
-                </option>)}
-            </select>
+            {Object.entries(foodFilterOptions).map(([key, value]) => <option key={key} value={key}>
+                {value}
+            </option>)}
+        </select>
         <input placeholder="Search By Name" value={searchedFood} onChange={(e) => setSearchedFood(e.target.value)}></input>
-        <button onClick={sortButtonHandler}>GO!</button>
         <ul className={classes.foodsToEatList}>
-            {Object.entries(foodChoices).map(([key, value]) => <EatFooodItemCard item={value} key={key} onFoodAdd={addFoodEatenHandler.bind(null, key)}>
+            {Object.entries(sortedItems).map(([key, value]) => <EatFooodItemCard item={value} key={key} onFoodAdd={addFoodEatenHandler.bind(null, key)}>
 
             </EatFooodItemCard>)}
         </ul>
@@ -97,4 +100,4 @@ const MealChoicesToEat = (props) => {
 
 };
 
-export default MealChoicesToEat;
+export default MealChoicesToEatTEST;
